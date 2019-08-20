2a66a6a4495e31dd85b561018e9c54eb9640483f
Kaspter Ju <camus@rtavs.com>
2019-08-20 18:01:22 +0800

    cmake: always enable git revision
    Signed-off-by: Kaspter Ju <camus@rtavs.com>

--- git_revision.cmake
+++ git_revision.cmake
@@ -1,15 +1,18 @@
-find_package(Git)
 
-if(GIT_FOUND)
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-list --max-count=1 HEAD
-        OUTPUT_VARIABLE GIT_REVISION
-        ERROR_QUIET
+# Find Git Version Patch
+IF(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+
+    find_package(Git)
+
+    if(GIT_FOUND)
+        execute_process(
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMAND ${GIT_EXECUTABLE} describe --always --tags --dirty
+            OUTPUT_VARIABLE GIT_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE
         )
-    if(NOT ${GIT_REVISION} STREQUAL "")
         string(STRIP ${GIT_REVISION} GIT_REVISION)
+        message(STATUS "Current git revision is ${GIT_REVISION}")
+    else()
+        set(GIT_REVISION "unknown")
     endif()
-    message(STATUS "Current git revision is ${GIT_REVISION}")
-else()
-    set(GIT_REVISION "unknown")
-endif()
+ENDIF()
